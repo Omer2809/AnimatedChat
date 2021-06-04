d9223f06916f2e35bab56fa5db2c17214ac41aa4
Mohd Omer <67154644+Omer2809@users.noreply.github.com>
2021-06-04 20:34:37 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
 
   return (
     <div className="app">
-      <div className="walkthrough">
+      <div className="wrap"><div className="walkthrough">
         {messages.map((message, index) => {
           const even = index % 2 === 0;
 
@@ -37,7 +37,7 @@ export default function App() {
       </div>
       <button className="btn" onClick={() => setMessageToShow(0)}>
         <MdReplay />
-      </button>
+      </button></div>
       <a href="https://github.com/Omer2809/AnimatedChat" target="_blank">
         <FaGithub className="git" />
       </a>
